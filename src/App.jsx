c78bad4e8af9e979547c2734212c0a988ae40fde
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-11-28 20:43:21 +0300

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
@@ -34,4 +33,4 @@ const App = () => {
   );
 };
 
-export default connect(null, null)(App);
+export default App;
